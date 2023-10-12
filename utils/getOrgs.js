dc260a677e8b3937ebef002630b7551d890e1544
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-10-12 15:37:55 +0200

    Setting up skeleton

---
+++ utils/getOrgs.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+let groupInfo = [];
+
+export async function getOrgs() {
+    async function getGroups() {
+    console.log("Getting Orgs")    
+    }
+  await getGroups();
+  
+  groupInfo = [{group: 'test'},{group: test2}]
+  
+  return groupInfo
+}
